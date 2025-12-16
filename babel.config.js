3c96b09e67f41ca3960a60342253deeef2907d8a
DarkBoy24 <darkboy241209@gmail.com>
2025-12-16 22:34:26 +0530

    C-1

---
+++ babel.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './src',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@utils': './src/utils',
+          '@models': './src/models',
+          '@constants': './src/constants',
+          '@config': './src/config',
+        },
+      },
+    ],
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+      },
+    ],
+  ],
+};
